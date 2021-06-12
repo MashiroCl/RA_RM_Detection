e340deed8cdd2e0c6e13508d5973c00597984d17
MashiroCl <441016116@qq.com>
2021-06-12 13:11:08 +0900

    recursion on getting commit lists(bug to be fixed)

--- RA2.0/CommitGraph.py
+++ RA2.0/CommitGraph.py
@@ -44,6 +44,7 @@ class CommmitGraph():
         p_lists=[]
         cc_list=[]
         p_list=[]
+
         for each in self.commits:
             if len(each.child)>2 or len(each.parentID)==2:
                 if len(cc_list)!=0:
@@ -60,6 +61,77 @@ class CommmitGraph():
                         cc_list = []
         return cc_lists
 
+    def test(self,commit:Commit,cc_list:list,cc_lists:list):
+
+        if ("4bf9a7f" in commit.commitID):
+            print("!!!!!!!!")
+
+        if (len(commit.parent) == 0):
+                cc_list.append(commit)
+                cc_lists.append(cc_list)
+                return 1
+
+        if len(commit.parent)==2:
+            if len(cc_list) != 0:
+                cc_lists.append(cc_list)
+                cc_list = []
+        elif len(commit.child)>2:
+            if len(cc_list) != 0:
+                cc_lists.append(cc_list)
+                cc_list = []
+
+            cc_list.append(commit)
+        else:
+                cc_list.append(commit)
+
+        # if len(commit.child)>2 or len(commit.parent)==2:
+        #         if len(cc_list)!=0:
+        #             cc_lists.append(cc_list)
+        #             cc_list=[]
+        #         elif len(commit.parent)==1:
+        #             cc_list.append(commit)
+        # else:
+        #     if len(cc_list) == 0:
+        #         cc_list.append(commit)
+        #     else:
+        #         if (self.isConnected(cc_list[-1],commit)):
+        #             cc_list.append(commit)
+        #         else:
+        #             cc_lists.append(cc_list)
+        #             cc_list = []
+
+        for each in commit.parent:
+            return self.test(each,cc_list,cc_lists)
+
+    def test2(self,commits:Commit,cc_list:list,cc_lists:list):
+
+        for commit in commits:
+            if (len(commit.parent) == 0):
+                    cc_list.append(commit)
+                    cc_lists.append(list(set(cc_list)))
+                    return 1
+
+            if len(commit.parent)==2:
+                if len(cc_list) != 0:
+                    cc_lists.append(cc_list)
+                    cc_list = []
+            elif len(commit.child)>2:
+                if len(cc_list) != 0:
+                    cc_lists.append(cc_list)
+                    cc_list = []
+
+                cc_list.append(commit)
+            else:
+                    cc_list.append(commit)
+
+        return self.test2(commit.parent,cc_list,cc_lists)
+
+
+def listSet(l):
+    temp=[]
+    for each in l:
+        temp.append(list(set(each)))
+    return temp
 
 if __name__=="__main__":
     path="/Users/leichen/ResearchAssistant/InteractiveRebase/data/jfinal/test.json"
@@ -72,14 +144,30 @@ if __name__=="__main__":
 
     cG=CommmitGraph(commits)
     # print(cG.whichCommit(cG.commits[0].parentID[0]).commitID)
-    cG.formGraph()
+    head=cG.formGraph()
     # print(cG.commits[cG.num-2].parent[0].commitID)
     # print(cG.whichCommit("d85b175db4ce63f102ff2071caa8655724aed537").child[0].commitID)
-    c=cG.SequenceCommit()
+    # c=cG.SequenceCommit()
+
+    cc_list = []
+    cc_lists = []
+
+
+    cG.test(head,cc_list,cc_lists)
+    # temp=[head]
+    # cG.test2(temp,cc_list,cc_lists)
+    # cc_lists=listSet(cc_lists)
+
+    # print(len(cG.whichCommit("a05da7f81debfa738c073c345d40815c5650da58").parent))
+    # for each in cG.whichCommit("a05da7f81debfa738c073c345d40815c5650da58").parent:
+    #     if each==None:
+    #         print("!!!!!!!")
+
     # print(cG.isConnected(cG.whichCommit("125c4b00bf6a7d5f70dadaeb1992caf4f5682a24"),
     #                      cG.whichCommit("f2de6d8bcabdafe213dc7ba96de43d620cecaca1")))
     # print(cG.isConnected(cG.whichCommit("125c4b00bf6a7d5f70dadaeb1992caf4f5682a24"),None))
-    for each1 in c:
+    # print(cc_lists)
+    for each1 in cc_lists:
         print("_________________________")
         for each2 in each1:
             print(each2.commitID)
